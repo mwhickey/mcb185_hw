e0088da97177df26edf974f1a47dc5fe7248759a
mwhickey <mwhickey@ucdavis.edu>
2025-01-28 15:21:36 -0800

    update

--- 11oligo.py
+++ 11oligo.py
@@ -1,6 +1,6 @@
 def oligotemp(A, C, G, T):
-	total_length = A + C + G + T
-	if total_length <= 13:
+	nt = A + C + G + T
+	if nt <= 13:
 		return (A + T) * 2 + (G + C) * 4
 	else: 
 		return 64.9 + 41 * (G + C - 16.4) / (A + T + G +C)
