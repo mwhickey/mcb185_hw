d6ac931ba397dc41d822e7f1fdafe82f5e78d219
mwhickey <mwhickey@ucdavis.edu>
2025-02-11 15:51:29 -0800

    new

--- au2.py
+++ au2.py
@@ -1,22 +1,16 @@
-import math
 
-def prime(n):
-	if n <= 1: 
-		return False
-	for n in range(2, int(n ** 0.5) + 1):
-		if n % 1 == 0:
-			return False
-		return True
+
+def is_prime(n):
+	for den in range(2, n//2):
+		if n % den == 0: return False
+	return True
 def prime51():
-	for i in range(51, 0, -2):
-		if prime:
-			print(i, '*')
-		else: print(i)
+	for n in range(51, 0, -2):
+		if is_prime(n):
+			print(n, '*')
+		else: print(n)
 prime51()
 
-
-		
-		
 	
 	
 	
