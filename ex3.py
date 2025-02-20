c43a54da3eeb2fbd6f66e374960d2527ec2ec087
mwhickey <mwhickey@ucdavis.edu>
2025-02-20 15:33:34 -0800

    update

---
+++ ex3.py
@@ -0,0 +1,20 @@
+import sys
+import math
+import statistics
+
+if len(sys.argv) > 1:
+	print('number of values:', len(sys.argv) - 1)
+	numbers = [float(arg) for arg in sys.argv[1:]]
+	print('Min:', min(numbers))
+	print('Max:', max(numbers))
+	print('Mean:', statistics.mean(numbers))
+	print('SD:', statistics.stdev(numbers))
+	print('Median:', statistics.median(numbers))
+	
+	mean = statistics.mean(numbers)
+	stdev = statistics.stdev(numbers)
+	
+	print("\nZ-scores:")
+	for num in numbers:
+		z_score = (num - mean) / stdev
+		print(f"Z-score for {num}: {z_score:.4f}")
