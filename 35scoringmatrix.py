2470730f7750b4a3ee7148082e06afbb2547ba6b
mwhickey <mwhickey@ucdavis.edu>
2025-02-23 13:01:37 -0800

    rewrite

--- 35scoringmatrix.py
+++ 35scoringmatrix.py
@@ -1,30 +1,21 @@
 import sys
-
-
-def scorematrix(alphabet, match, mismatch):
-	header = " " + " ".join(alphabet)
-	print(header)
-
-	for row in alphabet: 
-		rowdata = [row]
-		for col in alphabet:
-			if row == col:
-				rowdata.append(str(match))
-			else: 
-				rowdata.append(str(mismatch))
-		print(" ".join(rowdata))
-
-def main():
-	if len(sys.argv) != 4:
-		sys.exit(1)
 	
-	alphabet = sys.argv[1]
-	try:
-		match = int(sys.argv[2])
-		mismatch = int(sys.argv[3])
-	except error:
-		sys.exit(1)
-		
-	scorematrix(alphabet, match, mismatch)
+	
+base = sys.argv[1]
+match = sys.argv[2]
+mismatch = sys.argv[3]
 
-main()
+	
+print("   " + "  ".join(base))
+
+for row in base: 
+	rowlist = []
+	rowlist.append(row)
+	for col in base:
+		if row == col:
+			rowlist.append(str(match))
+		else: 
+			rowlist.append(str(mismatch))
+	print(" ".join(rowlist))
+
+
