b1be553132fc94fbeb4df75adc97001d32aaac7c
mwhickey <mwhickey@ucdavis.edu>
2025-03-20 12:16:21 -0700

    update

--- 48transmembrane.py
+++ 48transmembrane.py
@@ -1,17 +1,73 @@
+import sys
+import mcb185
+import sequence
 
-
-
-# alpha helices do not contain proline(P); signal peptide / transmembrane regions
-# signal peptide: 8 aa long, average KD >= 2.5, first 30 aa
-# transmembrane region: 11 aa long, average KD >= 2/0, after 30 aa
 kdh = {
 	'I':  4.5, 'V':  4.2, 'L':  3.8, 'F':  2.8, 'C':  2.5,
 	'M':  1.9, 'A':  1.8, 'G': -0.4, 'T': -0.7, 'S': -0.8,
 	'W': -0.9, 'Y': -1.3, 'P': -1.6, 'H': -3.2, 'E': -3.5,
 	'Q': -3.5, 'D': -3.5, 'K': -3.9, 'N': -3.5, 'R': -4.5,
+	'*': 0
 }
 def hydropathy(seq):
 	s = 0
 	for aa in seq:
 		s += kdh[aa]
 	return s / len(seq)
+	
+gcode = {
+	'AAA' : 'K',	'AAC' : 'N',	'AAG' : 'K',	'AAT' : 'N',
+	'ACA' : 'T',	'ACC' : 'T',	'ACG' : 'T',	'ACT' : 'T',
+	'AGA' : 'R',	'AGC' : 'S',	'AGG' : 'R',	'AGT' : 'S',
+	'ATA' : 'I',	'ATC' : 'I',	'ATG' : 'M',	'ATT' : 'I',
+	'CAA' : 'Q',	'CAC' : 'H',	'CAG' : 'Q',	'CAT' : 'H',
+	'CCA' : 'P',	'CCC' : 'P',	'CCG' : 'P',	'CCT' : 'P',
+	'CGA' : 'R',	'CGC' : 'R',	'CGG' : 'R',	'CGT' : 'R',
+	'CTA' : 'L',	'CTC' : 'L',	'CTG' : 'L',	'CTT' : 'L',
+	'GAA' : 'E',	'GAC' : 'D',	'GAG' : 'E',	'GAT' : 'D',
+	'GCA' : 'A',	'GCC' : 'A',	'GCG' : 'A',	'GCT' : 'A',
+	'GGA' : 'G',	'GGC' : 'G',	'GGG' : 'G',	'GGT' : 'G',
+	'GTA' : 'V',	'GTC' : 'V',	'GTG' : 'V',	'GTT' : 'V',
+	'TAA' : '*',	'TAC' : 'Y',	'TAG' : '*',	'TAT' : 'Y',
+	'TCA' : 'S',	'TCC' : 'S',	'TCG' : 'S',	'TCT' : 'S',
+	'TGA' : '*',	'TGC' : 'C',	'TGG' : 'W',	'TGT' : 'C',
+	'TTA' : 'L',	'TTC' : 'F',	'TTG' : 'L',	'TTT' : 'F',
+}
+def translate(seq):
+	pro = []
+	for i in range(0, len(seq), 3):
+		codon = seq[i:i+3]
+		if codon in gcode: aa = gcode[codon]
+		else:              aa = '*'
+		pro.append(aa)
+		#if aa == '*': break
+	return ''.join(pro)
+
+for defline, seq in mcb185.read_fasta(sys.argv[1]):
+	print(defline)
+	transseq = translate(seq)
+	signal_peptide_seq = transseq[:30]
+	signal_peptide = False
+	for i in range(len(signal_peptide_seq) - 7):
+		tempseq = signal_peptide_seq[i: i + 8]
+		tempkd = hydropathy(tempseq)
+		if tempkd >= 2.5: 
+			signal_peptide = True
+			break
+
+	tmseq = transseq[30:]
+	tm = False
+	for i in range(len(tmseq) - 10):
+		tempseq = tmseq[i: i + 11]
+		tempkd = hydropathy(tempseq)
+		if tempkd >= 2.5: 
+			tm = True
+			break
+	
+	print("single_pep:", signal_peptide, "|tm:",tm)
+	if signal_peptide == True and tm == True:
+		print(defline)
+
+
+	
+		
